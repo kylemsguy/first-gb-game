8f0d037fe83c32cd23d4a233a3bf3ceb3ca6695a
Kyle <kylezhou2002@hotmail.com>
2016-06-19 01:10:14 -0400

    Improved demo track

--- main.asm
+++ main.asm
@@ -4,24 +4,24 @@ INCLUDE "header.inc" ; Include cartrdige boot header
 
 ; $0150: Code!
 main:
-	ld A, $FF ;
-	ldh [$24], A ; set volume
+	;ld A, $FF ;
+	;ldh [$24], A ; set volume
 
-	ldh [$25], A ; enable channels
+	;ldh [$25], A ; enable channels
 
-	ld A, $80 ;
-	ldh [$26], A ; enable sound channels
+	;ld A, $80 ;
+	;ldh [$26], A ; enable sound channels
 
-	ldh [$23], A ; disable counter
+	;ldh [$23], A ; disable counter
 
 	;ld A, $F0
 	;ldh [$21], A ; set noise volume and disable envelope
 
-	ld A, $11
-	ldh [$06], A ; load TMA
+	;ld A, $11
+	;ldh [$06], A ; load TMA
 
-	ld A, $04
-	ldh [$07], A ; load TAC
+	;ld A, $04
+	;ldh [$07], A ; load TAC
 
 	call MUSIC_INIT
 
@@ -40,4 +40,4 @@ joypad:
     reti
 
 SECTION   "CoolStuff",ROM0[$0500]
-INCBIN "zinnia.gbs", $70, $4d9
+INCBIN "zinnia.gbs", $70, $1d89
